2a00c6ab61a8dd1a81701a10e0581a644e937f5e
minar <minar.tastic@gmail.com>
2019-06-18 12:03:12 +0200

    Base files

---
+++ tarazed.js
@@ -0,0 +1,7 @@
+// Separate script to render the visualization
+// D3 is imported in the html file, where this script is executed
+
+// Some useful variables, all in the same place to simplify the configuration
+var id_to_select = "#tarazed";
+var width = 450;
+var height = 450;
