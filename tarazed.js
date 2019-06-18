f1000ea3b133e9f093bccd1011c8e1e3efdcef23
minar <minar.tastic@gmail.com>
2019-06-18 16:58:40 +0200

    Take a json containing every space launch in history

--- tarazed.js
+++ tarazed.js
@@ -5,3 +5,16 @@
 var id_to_select = "#tarazed";
 var width = 450;
 var height = 450;
+var iterations = 1;
+var limit = 3000; // Takes this number of launches
+var base_url = "https://launchlibrary.net/1.4/launch?startdate=1950-01-01";
+var final_page_url = base_url + "&limit=" + limit;
+
+// Get a gigantic json containing every launch in history
+d3.json(final_page_url, function (error, json) {
+    process_data(json);
+});
+
+function process_data (json) {
+    return;
+}
