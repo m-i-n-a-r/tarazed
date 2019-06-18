ead9bb1c5c29ccc48ed374991e028f1d5ff07f2e
minar <minar.tastic@gmail.com>
2019-06-18 17:31:02 +0200

    Camelcase + some processing

--- tarazed.js
+++ tarazed.js
@@ -2,19 +2,23 @@
 // D3 is imported in the html file, where this script is executed
 
 // Some useful variables, all in the same place to simplify the configuration
-var id_to_select = "#tarazed";
+var idToSelect = ["#tarazed1"];
 var width = 450;
 var height = 450;
 var iterations = 1;
 var limit = 3000; // Takes this number of launches
-var base_url = "https://launchlibrary.net/1.4/launch?startdate=1950-01-01";
-var final_page_url = base_url + "&limit=" + limit;
+var baseUrl = "https://launchlibrary.net/1.4/launch?startdate=1950-01-01";
+var finalPageUrl = baseUrl + "&limit=" + limit;
 
 // Get a gigantic json containing every launch in history
-d3.json(final_page_url, function (error, json) {
-    process_data(json);
+d3.json(finalPageUrl, function (error, json) {
+    processData(json);
 });
 
-function process_data (json) {
-    return;
+function processData (json) {
+    // Take the launches in an array of objects, store some useful variables and build one or more graphs
+    let launches = json.launches;
+    let launchesNumber = launches.length - 1
+    let lastLaunchYear = parseInt(launches[launchesNumber].windowend.substring(0,4)); 
+    console.log(lastLaunchYear);
 }
