7b6f818623b8ac6cf16b51490a7b3c9462b85660
m-i-n-a-r <glucaconti.952@gmail.com>
2019-06-20 14:58:46 +0200

    Update README.md, minor changes

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
-# Tarazed
+# Tarazed - History of space launches
 
 ### Status: v 0.1 - WIP
 
 ## Introduction
-Tarazed simply takes some data from https://launchlibrary.net and display them using d3 and javascript. This kind of visualization should be easy to embed in another web page. 
+Tarazed takes every **space launch** in history from [Launchlibrary](https://launchlibrary.net) and displays a visual representation using **d3js**, **javascript** and other libraries. This kind of visualization should be easy to embed in another web page (an example wrapper is included). 
+
+## Task
+> How can a user understand the progression behind each space agency and the overall evolution of the space travel phenomenon?
+
+The specific task is to display the history of the space travels from 1961 until the last scheduled launch in a clear and easy to understand representation. The user can see the progress of each agency on a time based graph, interacting with the graph to highlight or aggregate data.
+
+## Features
+- Dynamic data! The visualization is **automatically updated** when a new launch is scheduled
+- Interactive visual representation, multiple options
+- The sources are easy to **customize** and understand
+- A beautiful **page template** to display the graph is included
+
+## How to
+Just take the script, import it in an html page, and make sure to add a div with the correct id (it must match the `idToSelect` variable in the js file). The customizable variables are in the script iteself. 
--- tarazed.js
+++ tarazed.js
@@ -3,22 +3,42 @@
 
 // Some useful variables, all in the same place to simplify the configuration
 var idToSelect = ["#tarazed1"];
-var width = 450;
+var width = 960;
 var height = 450;
 var iterations = 1;
-var limit = 3000; // Takes this number of launches
+var limit = 3000; // Takes up to this number of launches
 var baseUrl = "https://launchlibrary.net/1.4/launch?startdate=1950-01-01";
 var finalPageUrl = baseUrl + "&limit=" + limit;
+var graphBgColor = "#000000"
 
 // Get a gigantic json containing every launch in history
 d3.json(finalPageUrl, function (error, json) {
-    processData(json);
+    // Process data focusing on the launches
+    processLaunches(json);
 });
 
-function processData (json) {
+function processLaunches(json) {
     // Take the launches in an array of objects, store some useful variables and build one or more graphs
     let launches = json.launches;
     let launchesNumber = launches.length - 1
-    let lastLaunchYear = parseInt(launches[launchesNumber].windowend.substring(0,4)); 
-    console.log(lastLaunchYear);
+    let firstLaunchYear = 1961;
+    let lastLaunchYear = parseInt(launches[launchesNumber].windowend.substring(0, 4));
+
+    // Create a stacked bar graph to display the launches over the years
+    // The main container, it should scale to fit the screen div size
+    var svg = d3.select(idToSelect[0]).append("svg")
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("viewBox", "0 0 " + width + " " + height);
+
+    // The background of the graph    
+    var bgPath = svg.append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("rx", 0)
+        .attr("ry", 0)
+        .attr("height", height)
+        .attr("width", width)
+        .attr("opacity", 1)
+        .style("fill", graphBgColor);
+
 }
