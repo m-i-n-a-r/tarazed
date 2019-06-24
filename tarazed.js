781ac93f4b034df5c36bc88fa5bd3d8e4a6f33ab
minar <minar.tastic@gmail.com>
2019-06-24 14:55:31 +0200

    Nevermind, eventdrops doesn't fit as expected

--- tarazed.js
+++ tarazed.js
@@ -1,11 +1,12 @@
 // Separate script to render the visualization
 // D3 is imported in the html file, where this script is executed
 
+const firstLaunchYear = 1961;
+
 // Some useful variables, all in the same place to simplify the configuration
 var idToSelect = ["#tarazed1"];
 var width = 1600;
 var height = 900;
-var iterations = 1;
 var limit = 3000; // Takes up to this number of launches
 var baseUrl = "https://launchlibrary.net/1.4/launch?startdate=1950-01-01";
 var finalPageUrl = baseUrl + "&limit=" + limit;
@@ -21,7 +22,6 @@ function processLaunches(json) {
     // Take the launches in an array of objects, store some useful variables and build one or more graphs
     let launches = json.launches;
     let launchesNumber = launches.length - 1
-    let firstLaunchYear = 1961;
     let lastLaunchYear = parseInt(launches[launchesNumber].windowend.substring(0, 4));
 
     // Create a stacked bar graph to display the launches over the years
