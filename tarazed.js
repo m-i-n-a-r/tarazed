7c2ad6465305d2c632c00d8891d0c4da02c71efd
minar <minar.tastic@gmail.com>
2019-06-25 12:12:07 +0200

    Fixes, data aggregation

--- tarazed.js
+++ tarazed.js
@@ -13,11 +13,13 @@ const margin = ({
 var idToSelect = ["#tarazed1"];
 var width = 1800;
 var height = 600;
-var loadingColor = "#333333";
+var height2 = 660;
+var loadingColor = "#aaaaaa";
 var loadingText = "...FETCHING RESULTS...";
 var spinnerRadius = 50;
 var limit = 3000; // Takes up to this number of launches
-var baseUrl = "https://launchlibrary.net/1.4/launch?mode=verbose"; // The only way to retrieve every needed parameter is using the verbose mode
+// The only way to retrieve every needed parameter is using the verbose mode (instead of list or summary)
+var baseUrl = "https://launchlibrary.net/1.4/launch?mode=list";
 var finalPageUrl = baseUrl + "&limit=" + limit;
 var graphBgColor = "#000000"
 
@@ -40,7 +42,7 @@ svg.append("text")
 var radius = spinnerRadius;
 var tau = 2 * Math.PI;
 var arc = d3.arc()
-    .innerRadius(radius * 0.5)
+    .innerRadius(radius * 0.7)
     .outerRadius(radius * 0.9)
     .startAngle(0);
 var spinner = svg.append("g")
@@ -48,15 +50,15 @@ var spinner = svg.append("g")
     .attr("id", "spinner")
     .attr("opacity", 0.9)
 var background = spinner.append("path")
-    .datum({ endAngle: 0.25 * tau })
-    .style("fill", "loadingColor")
+    .datum({ endAngle: 0.75 * tau })
+    .style("fill", loadingColor)
     .attr("d", arc)
     .call(spin, 1500)
 function spin(selection, duration) {
     selection.transition()
         .duration(duration)
         .attrTween("transform", function () {
-            return d3.interpolateString("rotate(0)", "rotate(360)");
+            return d3.interpolateString("rotate(45)", "rotate(405)");
         });
     setTimeout(function () { spin(selection, duration); }, duration);
 }
@@ -70,12 +72,25 @@ function transitionFunction(path) {
 // Get a gigantic json containing every launch in history
 d3.json(finalPageUrl, json => {
     // Process data focusing on the launches
-    const processedData = processData(json);
-    drawChart1(processedData);
+    //const processedData = processData(json);
+    // Aggregate data on a monthly basis
+    const monthlyAggregateData = aggregateData(json);
+    drawChartAggregate(aggregateData);
 });
+
+var aggregateData = function (data) {
+    const launches = data.launches;
+    console.log(launches);
+    // Aggregate data on a month basis
+    aggregate = d3.nest()
+        .key(function (d) { return d.windowstart.substring(0, 7); })
+        .entries(launches);
+    return aggregate;
+}
 
 var processData = function (data) {
     const launches = data.launches;
+
     // Prepare to process dates
     const dateParse = d3.timeParse("%Y%m%dT%H%M%S%Z");
     const dateFormat = d3.timeFormat("%Y-%m-%d");
@@ -87,7 +102,7 @@ var processData = function (data) {
     const lastLaunchYear = parseInt(launches[launchesNumber].windowend.substring(0, 4));
 
     return launches.map((row) => {
-        // Pick out a subset of the interesting properties
+        // Pick a subset of interesting properties
         return {
             name: row.name,
             net: dateParse(row.isonet), // Convert ISO timestamp to Javascript Dates
@@ -99,75 +114,125 @@ var processData = function (data) {
             numAgencies: checkAgencies(row.rocket.agencies)
         }
     });
+
 }
 
-var drawChart1 = function (data) {
-    // Create a stacked bar graph to display the launches over the years
+var drawChartAggregate = function (data) {
+    var parseDate = d3.timeParse("%b %Y");
 
-    // Remove the "loading" text
-    d3.select("text").transition("removeLoadingText")
-        .duration(500)
-        .attr("opacity", 0)
-        .remove();
-    d3.select("#spinner").transition("removeLoadingSpinner")
-        .duration(500)
-        .attr("opacity", 0)
-        .remove();
+    var x = d3.scaleTime().range([0, width]),
+        x2 = d3.scaleTime().range([0, width]),
+        y = d3.scaleLinear().range([height, 0]),
+        y2 = d3.scaleLinear().range([height2, 0]);
 
-    // The background of the graph    
-    var bgPath = svg.append("rect")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("rx", 0)
-        .attr("ry", 0)
-        .attr("height", height)
-        .attr("width", width)
-        .attr("opacity", 0)
-        .style("fill", graphBgColor);
+    var xAxis = d3.axisBottom(x),
+        xAxis2 = d3.axisBottom(x2),
+        yAxis = d3.axisLeft(y);
 
-    const xRange = [margin.left, width - margin.right];
-    const xScale = d3.scaleBand()
-        .domain(data.map(d => d.name))
-        .range(xRange)
-        .padding(0.15); // how much space to put around each bar
+    var brush = d3.brushX()
+        .extent([[0, 0], [width, height2]])
+        .on("brush end", brushed);
 
-    const maxAgencies = d3.max(data.map(d => d.numAgencies));
-    const numAgenciesRange = [0, maxAgencies];
-    const yRange = [height - margin.bottom, margin.top].map(d => d / 1); // scaling
-    const yScale = d3.scaleLinear()
-        .domain(numAgenciesRange)
-        .range(yRange)
-        .nice();
+    var zoom = d3.zoom()
+        .scaleExtent([1, Infinity])
+        .translateExtent([[0, 0], [width, height]])
+        .extent([[0, 0], [width, height]])
+        .on("zoom", zoomed);
 
-    // Draw the chart
-    var barColor = function (row) {
-        if (row.location.countryCode === "USA") {
-            return "#78CDD7"; // pale turqoise saved from earlier
-        } else {
-            return "lightgrey";
-        }
-    };
+    var area = d3.area()
+        .curve(d3.curveMonotoneX)
+        .x(function (d) { return x(d.date); })
+        .y0(height)
+        .y1(function (d) { return y(d.price); });
 
-    const yAxis = g => g
-        .attr("transform", `translate(${margin.left}, 0)`)
-        .call(d3.axisLeft(yScale) // place tickets to the left of the bar
-            .ticks(maxAgencies)
-            // .tickSize(0)
-            .tickFormat(d3.format("d"))
-        )
+    var area2 = d3.area()
+        .curve(d3.curveMonotoneX)
+        .x(function (d) { return x2(d.date); })
+        .y0(height2)
+        .y1(function (d) { return y2(d.price); });
 
-    // Build the final chart
-    svg.append("g")
-        .selectAll("rect")
-        .data(data)
-        .enter()
+    svg.append("defs").append("clipPath")
+        .attr("id", "clip")
         .append("rect")
-        .attr("x", d => xScale(d.name))
-        .attr("y", d => yScale(d.numAgencies))
-        .attr("height", d => yScale(0) - yScale(d.numAgencies))
-        .attr("width", xScale.bandwidth() / 2)
-        .attr("fill", barColor);
+        .attr("width", width)
+        .attr("height", height);
 
-    svg.append("g")
-        .call(yAxis);
+    var focus = svg.append("g")
+        .attr("class", "focus")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var context = svg.append("g")
+        .attr("class", "context")
+        .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+
+    d3.csv("sp500.csv", type, function (error, data) {
+        if (error) throw error;
+
+        x.domain(d3.extent(data, function (d) { return d.date; }));
+        y.domain([0, d3.max(data, function (d) { return d.price; })]);
+        x2.domain(x.domain());
+        y2.domain(y.domain());
+
+        focus.append("path")
+            .datum(data)
+            .attr("class", "area")
+            .attr("d", area);
+
+        focus.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        focus.append("g")
+            .attr("class", "axis axis--y")
+            .call(yAxis);
+
+        context.append("path")
+            .datum(data)
+            .attr("class", "area")
+            .attr("d", area2);
+
+        context.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + height2 + ")")
+            .call(xAxis2);
+
+        context.append("g")
+            .attr("class", "brush")
+            .call(brush)
+            .call(brush.move, x.range());
+
+        svg.append("rect")
+            .attr("class", "zoom")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+            .call(zoom);
+    });
+
+    function brushed() {
+        if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
+        var s = d3.event.selection || x2.range();
+        x.domain(s.map(x2.invert, x2));
+        focus.select(".area").attr("d", area);
+        focus.select(".axis--x").call(xAxis);
+        svg.select(".zoom").call(zoom.transform, d3.zoomIdentity
+            .scale(width / (s[1] - s[0]))
+            .translate(-s[0], 0));
+    }
+
+    function zoomed() {
+        if (d3.event.sourceEvent && d3.event.sourceEvent.type === "brush") return; // ignore zoom-by-brush
+        var t = d3.event.transform;
+        x.domain(t.rescaleX(x2).domain());
+        focus.select(".area").attr("d", area);
+        focus.select(".axis--x").call(xAxis);
+        context.select(".brush").call(brush.move, x.range().map(t.invertX, t));
+    }
+
+    function type(d) {
+        d.date = parseDate(d.date);
+        d.price = +d.price;
+        return d;
+    }
 }
